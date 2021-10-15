66e1e6c73817cc14251f4a5d00dbddefd285033e
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2021-10-16 00:26:51 +0200

    Added javadoc

--- app/src/main/java/com/basilalasadi/fasters/math/PrayerTimings.java
+++ app/src/main/java/com/basilalasadi/fasters/math/PrayerTimings.java
@@ -4,6 +4,10 @@ import org.threeten.bp.ZonedDateTime;
 import static com.basilalasadi.fasters.math.AstronomyMath.*;
 
 
+/**
+ * <p>Abstract class for calculating prayer times.</p>
+ * @see AstronomyMath
+ */
 @SuppressWarnings({"SpellCheckingInspection", "unused"})
 public abstract class PrayerTimings {
 	
@@ -39,6 +43,11 @@ public abstract class PrayerTimings {
 	private static final String COUNTRY_RUSSIA                = "Russia";
 	
 	
+	/**
+	 * <p>Get default method (enum) for country.</p>
+	 * @param country country name
+	 * @return the default method (enum) for country
+	 */
 	public static Method getMethodForCountry(String country) {
 		switch (country) {
 			case COUNTRY_UNITED_STATES:
@@ -65,6 +74,11 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Get the sun angle at fajr.</p>
+	 * @param method method enum.
+	 * @return sun angle in degrees.
+	 */
 	public static double getFajrAngleDegrees(Method method) {
 		switch (method) {
 			case IslamicSocietyOfNorthAmerica:
@@ -97,6 +111,11 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Get the sun angle at isha.</p>
+	 * @param method method enum.
+	 * @return sun angle in degrees.
+	 */
 	public static double getIshaAngleDegrees(Method method) {
 		switch (method) {
 			case IslamicSocietyOfNorthAmerica:
@@ -124,6 +143,12 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Get the fixed time offset from sunset to isha.</p>
+	 * @param method method enum.
+	 * @param isRamadan <em>true</em> if and only if current month is ramadan.
+	 * @return time offset in minutes, or <em>-1</em> if method doesn't have a fixed offset.
+	 */
 	public static double getIshaFixedOffset(Method method, boolean isRamadan) {
 		switch (method) {
 			case GulfRegion:
@@ -143,6 +168,11 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Get whether method (enum) has a fixed time offset for isha.</p>
+	 * @param method method enum.
+	 * @return <em>true</em> if and only if the method has a fixed time offset for isha.
+	 */
 	public static boolean getHasFixedIshaOffset(Method method) {
 		switch (method) {
 			case GulfRegion:
@@ -155,6 +185,15 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Get fajr time.</p>
+	 * @param angleDegrees sun angle at fajr.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return fajr time in minutes from 00:00.
+	 */
 	public static double getFajr(double angleDegrees, double daysSinceEpoch, int timeZone, double longitude, double latitude) {
 		double noon = localSolarNoon(daysSinceEpoch, timeZone, longitude);
 		double offset = noonOffsetFromSunAngle(angleDegrees, daysSinceEpoch, latitude);
@@ -162,14 +201,39 @@ public abstract class PrayerTimings {
 		return noon - offset;
 	}
 	
+	/**
+	 * <p>Get fajr time.</p>
+	 * @param method method enum.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return fajr time in minutes from 00:00.
+	 */
 	public static double getFajr(Method method, double daysSinceEpoch, int timeZone, double longitude, double latitude) {
 		return getFajr(getFajrAngleDegrees(method), daysSinceEpoch, timeZone, longitude, latitude);
 	}
 	
+	/**
+	 * <p>Get duhr time.</p>
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @return duhr time in minutes from 00:00.
+	 */
 	public static double getDuhr(double daysSinceEpoch, int timeZone, double longitude) {
 		return localSolarNoon(daysSinceEpoch, timeZone, longitude);
 	}
 	
+	/**
+	 * <p>Get asr time.</p>
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @param useShafaiMethod use shafai method instead (shadow ratio of 2 instead of 1).
+	 * @return asr time in minutes from 00:00.
+	 */
 	public static double getAsr(double daysSinceEpoch, int timeZone, double longitude, double latitude,
 			boolean useShafaiMethod) {
 		
@@ -181,12 +245,29 @@ public abstract class PrayerTimings {
 		return noon + offset;
 	}
 	
+	/**
+	 * <p>Get magrib time.</p>
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return magrib time in minutes from 00:00.
+	 */
 	public static double getMagrib(double daysSinceEpoch, int timeZone, double longitude, double latitude) {
 		double noon = localSolarNoon(daysSinceEpoch, timeZone, longitude);
 		double offset = noonOffsetFromSunAngle(0.833, daysSinceEpoch, latitude);
 		return noon + offset;
 	}
 	
+	/**
+	 * <p>Get isha time from sun angle.</p>
+	 * @param angleDegrees sun angle at isha.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return isha time in minutes from 00:00.
+	 */
 	public static double getIsha(double angleDegrees, double daysSinceEpoch, int timeZone,
 			double longitude, double latitude) {
 		
@@ -195,6 +276,15 @@ public abstract class PrayerTimings {
 		return noon + offset / 60;
 	}
 	
+	/**
+	 * <p>Get isha time from fixed time offset.</p>
+	 * @param offsetFromSunsetMinutes fixed time offset from magrib.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return isha time in minutes from 00:00.
+	 */
 	public static double getIsha(int offsetFromSunsetMinutes, double daysSinceEpoch, int timeZone,
 			double longitude, double latitude) {
 		
@@ -203,6 +293,16 @@ public abstract class PrayerTimings {
 		return noon + magribOffset + offsetFromSunsetMinutes / 60d;
 	}
 	
+	/**
+	 * <p>Get isha time using method (enum).</p>
+	 * @param method method enum.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @param isRamadan whether it is ramadan at specified <em>daysSinceEpoch</em>.
+	 * @return isha time in minutes from 00:00.
+	 */
 	public static double getIsha(Method method, double daysSinceEpoch, int timeZone,
 			double longitude, double latitude, boolean isRamadan) {
 		
@@ -216,6 +316,17 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Get all timings for day specified by <em>daysSinceEpoch</em>.</p>
+	 * @param fajrAngleDegrees sun angle at fajr.
+	 * @param useShafaiMethod use shafai method instead (shadow ratio of 2 instead of 1).
+	 * @param ishaAngleDegrees sun angle at isha.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return timings array in minutes since 00:00.
+	 */
 	public static double[] getTimings(double fajrAngleDegrees, boolean useShafaiMethod, double ishaAngleDegrees,
 			double daysSinceEpoch, int timeZone, double longitude, double latitude) {
 		
@@ -228,6 +339,17 @@ public abstract class PrayerTimings {
 		return new double[]{ fajr, noon, asr, magrib, isha };
 	}
 	
+	/**
+	 * <p>Get all timings for day specified by <em>daysSinceEpoch</em>.</p>
+	 * @param fajrAngleDegrees sun angle at fajr.
+	 * @param useShafaiMethod use shafai method instead (shadow ratio of 2 instead of 1).
+	 * @param ishaTimeOffsetMinutes fixed time offset from magrib in minutes.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @return timings array in minutes since 00:00.
+	 */
 	public static double[] getTimings(double fajrAngleDegrees, boolean useShafaiMethod, int ishaTimeOffsetMinutes,
 			double daysSinceEpoch, int timeZone, double longitude, double latitude) {
 		
@@ -240,6 +362,17 @@ public abstract class PrayerTimings {
 		return new double[]{ fajr, noon, asr, magrib, isha };
 	}
 	
+	/**
+	 * <p>Get all timings for day specified by <em>daysSinceEpoch</em> and method (enum).</p>
+	 * @param method method enum.
+	 * @param daysSinceEpoch days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 * @param timeZone time zone offset from UTC.
+	 * @param longitude longitude in degrees.
+	 * @param latitude latitude in degrees.
+	 * @param isRamadan whether it is ramadan at specified <em>daysSinceEpoch</em>.
+	 * @param useShafaiMethod use shafai method instead (shadow ratio of 2 instead of 1).
+	 * @return timings array in minutes since 00:00.
+	 */
 	public static double[] getTimings(Method method, double daysSinceEpoch, int timeZone,
 			double longitude, double latitude, boolean isRamadan, boolean useShafaiMethod) {
 		
@@ -255,6 +388,11 @@ public abstract class PrayerTimings {
 		}
 	}
 	
+	/**
+	 * <p>Convert zoned datetime to days since epoch.</p>
+	 * @param date zoned datetime.
+	 * @return days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 */
 	public static double toDaysSinceEpoch2000(ZonedDateTime date) {
 		return daysSinceEpoch(date.getYear(),
 				date.getMonthValue(),
@@ -264,6 +402,10 @@ public abstract class PrayerTimings {
 				date.getOffset().getTotalSeconds() / 3600);
 	}
 	
+	/**
+	 * <p>Converts current zoned datetime to days since epoch.</p>
+	 * @return days (and fraction of day) since 2000-01-01 00:00 UTC.
+	 */
 	public static double toDaysSinceEpoch2000() {
 		return toDaysSinceEpoch2000(ZonedDateTime.now());
 	}
